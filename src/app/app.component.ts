d6fa15d5ba9d58d2e5ac703903da3ed7c3fbd17e
Antifreez <antifreez@gmail.com>
2017-09-04 18:03:48 +0300

    Product table build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+
 @Component ({
   selector: 'pm-root',
   template: `
     <div>
       <h1>{{pageTitle}}</h1>
-      <div>My first Component</div>
+      <pm-products></pm-products>
     </div>
   `
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule
