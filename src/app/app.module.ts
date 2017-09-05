0c61af527fc8b26cf827eebc3710f92098f41c24
Antifreez <antifreez@gmail.com>
2017-09-05 12:36:13 +0300

    iterfaces & products filter implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductlistComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductlistComponent
+    ProductlistComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
