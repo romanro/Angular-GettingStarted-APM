652dee85606026d31d69bd89c5a57f4c9e27533a
Antifreez <antifreez@gmail.com>
2017-09-06 19:23:27 +0300

    addad back button

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
+   // console.log (this._route.snapshot);
+    let id = +this._route.snapshot.paramMap.get('id'); // + for convert string to number
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: 'aaaa',
+      productCode: 'ddd',
+      releaseDate: 'adada',
+      description: 'dsd',
+      price: 19,
+      starRating: 3.2,
+      imageUrl: 'aaa'
+    };
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
+
