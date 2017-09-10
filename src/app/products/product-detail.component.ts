4d7fd97089ed8e8236fe9a0783a56383f0189479
Antifreez <antifreez@gmail.com>
2017-09-10 18:45:45 +0300

    basic product-details template added

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,32 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  errorMessage: string = '';
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private _route: ActivatedRoute, private _router: Router) { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router,
+              private _productService: ProductService) { }
 
   ngOnInit() {
    // console.log (this._route.snapshot);
     const id = +this._route.snapshot.paramMap.get('id'); // + for convert string to number
     this.pageTitle += `: ${id}`;
-    this.product = {
-      productId: id,
-      productName: 'aaaa',
-      productCode: 'ddd',
-      releaseDate: 'adada',
-      description: 'dsd',
-      price: 19,
-      starRating: 3.2,
-      imageUrl: 'aaa'
-    };
+
+    this.getProduct(id);
+
   }
+
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe(
+      (product) => {
+        this.product = product;
+        // console.log (this.product);
+      },
+      error => this.errorMessage = <any>error);
+
+  }
+
 
   onBack(): void {
     this._router.navigate(['/products']);
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,12 +5,12 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 import { IProduct } from './product';
 
 
 @Injectable()
-
 export class ProductService {
 
     private _productUrl = './api/products/products.json';
@@ -20,12 +20,28 @@ export class ProductService {
     getProducts(): Observable<IProduct[]> {
         // console.log(this._http.get<IProduct[]>(this._productUrl));
         return this._http.get<IProduct[]>(this._productUrl)
-            .do (data => console.log(data))
+            // .do (data => console.log(data))
             .catch(this.handleError);
     }
 
-    private handleError (err: HttpErrorResponse) {
-        console.log(err.message);
-        return Observable.throw(err.message);
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage = '';
+        if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return Observable.throw(errorMessage);
     }
 }
